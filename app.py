28018c4832beac79c29b90f389b834e5f61169bd
Harshit jain <167976063+Harshit-jain-1@users.noreply.github.com>
2025-08-20 12:13:35 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyM+g3WQv5cJWIPSHeQgxFnu",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
