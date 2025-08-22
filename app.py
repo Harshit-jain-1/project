82bb17a6224ccbc444277c91fe461ec0c25d7bd2
Harshit jain <167976063+Harshit-jain-1@users.noreply.github.com>
2025-08-22 14:57:24 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/10PxcgO5FADLWop7whV-nQNxydZm18kGi
 """
 
-! pip install streamlit
 
 import streamlit as st
 import yfinance as yf
